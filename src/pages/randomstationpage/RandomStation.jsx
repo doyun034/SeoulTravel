2b473d8bf306e5361e2675da96841f2787b1c83e
Doyun Jang <ehdbsdl0304@gmail.com>
2024-11-13 17:35:29 +0900

    랜덤 지하철 돌리기 초안

--- src/pages/randomstationpage/RandomStation.jsx
+++ src/pages/randomstationpage/RandomStation.jsx
@@ -1,34 +1,70 @@
-import React, { useState } from 'react';
-import './RandomStation.css';
+import React, { useState, useEffect } from 'react';
+import '../../styles/RandomStation.css';
 import stations from './stations_data';
 
 const RandomStation = () => {
   const [selectedStation, setSelectedStation] = useState('');
   const [savedStations, setSavedStations] = useState([]);
+  const [availableStations, setAvailableStations] = useState([]);
+  const [selectedIcons, setSelectedIcons] = useState([]); // 선택된 노선 아이콘들 경로 배열
+  const [isAnimating] = useState(false); // 애니메이션 비활성화 상태
+
+
+  useEffect(() => {
+    setAvailableStations(Object.values(stations).flat());
+  }, []);
 
   const handleRandomStation = () => {
-    
-    // 저장되지 않은 역들만 필터링
-    const availableStations = Object.values(stations)
-      .flat()
-      .filter((station) => !savedStations.includes(station));
-
-    if (availableStations.length === 0) {
-      alert('모든 역이 저장되었습니다.');
-      return;
-    }
-
-    // 랜덤으로 새로운 역 선택
-    const randomStation =
-      availableStations[Math.floor(Math.random() * availableStations.length)];
-    setSelectedStation(randomStation);
+    const diceElement = document.querySelector('.dice-icon');
+  
+    diceElement.classList.add('animate');
+  
+    // 애니메이션 시간 (1초) 후에 랜덤 역 선택
+    setTimeout(() => {
+      diceElement.classList.remove('animate');
+  
+      const filteredStations = availableStations.filter(
+        (station) => !savedStations.includes(station.name)
+      );
+  
+      if (filteredStations.length === 0) {
+        alert('모든 역이 저장되었습니다.');
+        return;
+      }
+  
+      const randomStation =
+        filteredStations[Math.floor(Math.random() * filteredStations.length)];
+      setSelectedStation(randomStation.name);
+  
+      // 선택된 역에 해당하는 모든 노선 아이콘 설정
+      const stationLines = [];
+      Object.keys(stations).forEach((lineKey) => {
+        stations[lineKey].forEach((station) => {
+          if (station.name === randomStation.name) {
+            stationLines.push(station.icon);
+          }
+        });
+      });
+      setSelectedIcons(stationLines);
+    }, 
+    1000);
   };
 
-  // 저장 후 선택된 역 초기화
   const handleSaveStation = () => {
     if (selectedStation && !savedStations.includes(selectedStation)) {
-      setSavedStations([...savedStations, selectedStation]);
+      setSavedStations((prevSavedStations) => [
+        ...prevSavedStations,
+        selectedStation,
+      ]);
+
+      setAvailableStations((prevAvailableStations) =>
+        prevAvailableStations.filter((station) => station.name !== selectedStation)
+      );
       setSelectedStation('');
+      setSelectedIcons([]);
+
+    } else if (savedStations.includes(selectedStation)) {
+      alert('이미 저장된 역입니다.');
     }
   };
 
@@ -44,26 +80,43 @@ const RandomStation = () => {
         </div>
         <div className="random-station-container">
           <h2 className="title">랜덤으로 떠나는 서울여행</h2>
+          <h5 className="description">주사위를 굴려보세요!</h5>
           <div className="dice-button-container">
-            <img
+          <img
               src="/images/dice.png"
-              alt="주사위 아이콘"
-              className="dice-icon"
+              alt="랜덤으로 역을 선택하는 주사위 아이콘"
+              className={`dice-icon ${isAnimating ? 'animate' : ''}`}
+              role="button"
+              aria-label="랜덤 역 선택"
+              onClick={handleRandomStation}
             />
-            <button onClick={handleRandomStation} className="random-btn">
-              주사위를 굴려보세요!
-            </button>
           </div>
           {selectedStation && (
-            <div className="station-info">
-              <div className="station-name">{selectedStation}</div>
-              <p>
-                이번 역은 <strong>{selectedStation}</strong> 입니다.
-              </p>
-              <button onClick={handleSaveStation} className="save-btn">
-                역 저장하기
-              </button>
-            </div>
+            <>
+              <div className="station-description">
+                <div className="station-name">{selectedStation}</div>
+              </div>
+              <div className="station-information">
+                <p>
+                  이번 역은 <strong>{selectedStation}</strong> 입니다.
+                </p>
+              </div>
+              <div className="station-icons">
+                {selectedIcons.map((icon, index) => (
+                  <img
+                    key={index}
+                    src={`/station_icon/${icon}`}
+                    alt={`${selectedStation} 아이콘`}
+                    className="line-icon"
+                  />
+                ))}
+              </div>
+              <div className="save-btn-container">
+                <button onClick={handleSaveStation} className="save-btn">
+                  역 저장하기
+                </button>
+              </div>
+            </>
           )}
         </div>
       </main>
